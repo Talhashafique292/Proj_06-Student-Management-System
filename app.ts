07393eb8bdbd12ea098518f2141ead3e44e1587f
Talhashafique292 <talhashafique292@gmail.com>
2023-11-07 23:27:19 +0500

    Initial commit

---
+++ app.ts
@@ -0,0 +1,68 @@
+class School{
+    name: string;
+    students: Student[] = [];
+    teachers: Teacher[] = [];
+
+    addStudent(stdObj: Student) {
+        this.students.push(stdObj)
+   
+    }   
+
+    addTeacher(tecObj: Teacher) {
+        this.teachers.push(tecObj)
+    }
+    
+    constructor(name: string) {
+        this.name = name;
+    }  
+}
+
+class Student{
+    name: string;
+    age: number;
+    schoolName: string;
+
+    constructor(name: string, age: number, schoolName: string) {
+        this.name = name;
+        this.age = age;
+        this.schoolName = schoolName;
+    }
+};
+
+class Teacher extends Student { 
+    email: string="";
+    contact: string= "";
+
+    addinfo(email: string, contact: string) {
+        this.email = email;
+        this.contact = contact;
+    }
+};
+
+let school1 = new School("IBA");
+let school2 = new School("BFC");
+
+let s1 = new Student("Talha", 13, school2.name)
+let s2 = new Student("Faisal", 15, school1.name)
+let s3 = new Student("Najam", 14, school2.name)
+
+let t1 = new Teacher("Ahmed", 40, school1.name);
+let t2 = new Teacher("Remat", 37, school2.name);
+let t3 = new Teacher("Sikandar", 44, school2.name);
+
+
+school1.addStudent(s2);
+school2.addStudent(s1);
+school2.addStudent(s3);
+
+school1.addTeacher(t1);
+school2.addTeacher(t2);
+school2.addTeacher(t3);
+
+t1.addinfo("abc@gmail.com", "12345");
+t2.addinfo("abcd@gmail.com", "123456");
+t3.addinfo("abcde@gmail.com", "1234567");
+
+console.log(school1);
+console.log(school2);
+
